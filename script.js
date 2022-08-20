d9a1795186fd02c50009056ff96db3490757a453
Maer costa <39601135+Aramunii@users.noreply.github.com>
2022-08-19 21:25:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -479,9 +479,9 @@ $(function () {
                     }
                 };
 
-            await   $.ajax(settings).done(function (response) {
+            await   $.ajax(settings).done(async function (response) {
 urlchall = response.url.shortLink
-                    copyStringToClipboard(`Estou te desafiando em *${artistName}* no SongGuess! clique no link abaixo e tente ganhar de mim! \n` + response.url.shortLink);
+                  await  copyStringToClipboard(`Estou te desafiando em *${artistName}* no SongGuess! clique no link abaixo e tente ganhar de mim! \n` + response.url.shortLink);
                 });
 
             },
